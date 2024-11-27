17965fd28b36a071ca2fcbfa145381697c5ed2f4
bnznamco <gabriele.baldi.01@gmail.com>
2024-11-27 17:14:27 +0100

    fix: added redirects for old urls for retrocompatibility

--- camomilla/urls.py
+++ camomilla/urls.py
@@ -1,4 +1,3 @@
-from django.shortcuts import redirect
 from django.urls import include, path
 from rest_framework import routers
 from importlib.util import find_spec
@@ -21,6 +20,7 @@ from camomilla.views import (
     MenuViewSet,
 )
 from camomilla.views.pages import fetch_page
+from camomilla.redirects import url_patterns as old_redirects
 
 router = routers.DefaultRouter()
 
@@ -30,18 +30,15 @@ router.register(r"contents", ContentViewSet, "camomilla-content")
 router.register(r"media", MediaViewSet, "camomilla-media")
 router.register(r"media-folders", MediaFolderViewSet, "camomilla-media_folders")
 router.register(r"pages", PageViewSet, "camomilla-pages")
-router.register(r"sitemap", PageViewSet, "camomilla-sitemap")
 router.register(r"users", UserViewSet, "camomilla-users")
 router.register(r"permissions", PermissionViewSet, "camomilla-permissions")
 router.register(r"menus", MenuViewSet, "camomilla-menus")
 
 urlpatterns = [
+    *old_redirects,
     path("", include(router.urls)),
     path("pages-router/", fetch_page),
     path("pages-router/<path:permalink>", fetch_page),
-    path(
-        "profiles/me/", lambda _: redirect("../../users/current/"), name="profiles-me"
-    ),
     path("token-auth/", CamomillaObtainAuthToken.as_view(), name="api_token"),
     path("auth/login/", CamomillaAuthLogin.as_view(), name="login"),
     path("auth/logout/", CamomillaAuthLogout.as_view(), name="logout"),
