d311032501f4069cd49e8ddb2e2e7ce45507c4f5
bnznamco <gabriele.baldi.01@gmail.com>
2024-11-27 13:27:06 +0100

    refactor: refactor camomilla theme admin code disposition

--- camomilla/theme/admin/pages.py
+++ camomilla/theme/admin/pages.py
@@ -1,20 +1,10 @@
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
-from django.contrib import admin
-from django.http import HttpResponse
 from modeltranslation.translator import translator
-
 from camomilla import settings
 from camomilla.utils.translation import get_field_translation_accessors
-
-if settings.ENABLE_TRANSLATIONS:
-    from modeltranslation.admin import (
-        TabbedTranslationAdmin as TranslationAwareModelAdmin,
-    )
-else:
-    from django.contrib.admin import ModelAdmin as TranslationAwareModelAdmin
-
-from camomilla.models import Article, Content, Media, MediaFolder, Page, Tag, Menu, UrlRedirect, UrlNode
+from .translations import TranslationAwareModelAdmin
+from camomilla.models import UrlNode
+from django.utils.translation import gettext_lazy as _
 
 
 class AbstractPageModelFormMeta(forms.models.ModelFormMetaclass):
@@ -59,7 +49,7 @@ class AbstractPageModelForm(
         if commit:
             model.save()
         return model
-
+    
 
 class AbstractPageAdmin(TranslationAwareModelAdmin):
     form = AbstractPageModelForm
@@ -73,86 +63,3 @@ class AbstractPageAdmin(TranslationAwareModelAdmin):
         for name, field in translator.get_options_for_model(UrlNode).fields.items():
             if name in fields_to_add:
                 self.trans_opts.fields.update({name: field})
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-
-
-class ArticleAdminForm(AbstractPageModelForm):
-    class Meta:
-        model = Article
-        fields = "__all__"
-        widgets = {"content": CKEditorUploadingWidget}
-
-
-class ArticleAdmin(AbstractPageAdmin):
-    filter_horizontal = ("tags",)
-    form = ArticleAdminForm
-
-
-class TagAdmin(TranslationAwareModelAdmin):
-    pass
-
-
-class MediaFolderAdmin(admin.ModelAdmin):
-    readonly_fields = ("path",)
-
-
-class ContentAdminForm(forms.ModelForm):
-    class Meta:
-        model = Content
-        fields = "__all__"
-        widgets = {"content": CKEditorUploadingWidget}
-
-
-class ContentAdmin(TranslationAwareModelAdmin):
-    form = ContentAdminForm
-
-
-class MediaAdmin(TranslationAwareModelAdmin):
-    exclude = (
-        "thumbnail",
-        "size",
-        "image_props",
-    )
-    readonly_fields = ("image_preview", "image_thumb_preview", "mime_type")
-    list_display = (
-        "__str__",
-        "title",
-        "image_thumb_preview",
-    )
-
-    def response_add(self, request, obj):
-        if request.GET.get("_popup", ""):
-            return HttpResponse(
-                """
-               <script type="text/javascript">
-                  opener.dismissAddRelatedObjectPopup(window, %s, '%s');
-               </script>"""
-                % (obj.id, obj.json_repr)
-            )
-        else:
-            return super(MediaAdmin, self).response_add(request, obj)
-
-
-class PageAdmin(AbstractPageAdmin):
-    pass
-
-
-class MenuAdmin(TranslationAwareModelAdmin):
-    pass
-
-
-class UrlRedirectAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(MediaFolder, MediaFolderAdmin)
-admin.site.register(Tag, TagAdmin)
-admin.site.register(Content, ContentAdmin)
-admin.site.register(Media, MediaAdmin)
-admin.site.register(Page, PageAdmin)
-admin.site.register(Menu, MenuAdmin)
-admin.site.register(UrlRedirect, UrlRedirectAdmin)
