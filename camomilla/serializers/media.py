4083d61553ce820886be47f7cc337a0684aeb7ae
Federico Di Filitto <federico.difilitto@protonmail.com>
2024-12-06 18:02:04 +0100

    fix(media): added check on media serializer for one to one fields

--- camomilla/serializers/media.py
+++ camomilla/serializers/media.py
@@ -1,9 +1,10 @@
+from django.db.models import Model
 from rest_framework import serializers
 
 from ..models import Media, MediaFolder
 from ..storages import OverwriteStorage
 from .base import BaseTranslatableModelSerializer
-
+from django.core.exceptions import ObjectDoesNotExist
 
 class MediaListSerializer(BaseTranslatableModelSerializer):
     is_image = serializers.SerializerMethodField("get_is_image")
@@ -28,18 +29,39 @@ class MediaSerializer(BaseTranslatableModelSerializer):
         result = []
         links = obj.get_foreign_fields()
         for link in links:
+            if not hasattr(obj, link):
+                # if the link is a one to one field,
+                # and this one to one field is not set,
+                # let's continue to the next relation
+                continue
+            
             manager = getattr(obj, link)
-            if hasattr(manager, "language"):
-                manager = manager.language()
-            for item in manager.all():
-                if item.__class__.__name__ != "MediaTranslation":
-                    result.append(
-                        {
-                            "model": item.__class__.__name__,
-                            "name": item.__str__(),
-                            "id": item.pk,
-                        }
-                    )
+
+            if isinstance(manager, Model):
+                # in this case it's not a manager but a one to one field
+                # so the manager is the actual single item
+                item = manager
+                result.append(                            {
+                    "model": item.__class__.__name__,
+                    "name": item.__str__(),
+                    "id": item.pk,
+                })
+            else: # otherwise it's a regular to many relationship
+                if hasattr(manager, "language"):
+                    manager = manager.language()
+
+                for item in manager.all():
+                    if item.__class__.__name__ != "MediaTranslation":
+                        result.append(
+                            {
+                                "model": item.__class__.__name__,
+                                "name": item.__str__(),
+                                "id": item.pk,
+                            }
+                        )
+
+
+                    
         return result
 
     def update(self, instance, data):
