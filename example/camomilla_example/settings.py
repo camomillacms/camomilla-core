3c48bfd64c62a8fb5d40a4a1c34439be35ec1cc8
bnznamco <gabriele.baldi.01@gmail.com>
2025-08-08 15:45:27 +0200

    chore: added django_debug_toolbar for development purposes

--- example/camomilla_example/settings.py
+++ example/camomilla_example/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
-
+import sys, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +26,10 @@ SECRET_KEY = "django-insecure-m2=e_)m)%$ks2d*txswt-b@*q1fns478smeo0u%=1$w)r38^kj
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+TESTING = "test" in sys.argv or "PYTEST_VERSION" in os.environ
+
+ENABLE_DEBUG_TOOLBAR = DEBUG and not TESTING
 
 ALLOWED_HOSTS = []
 
@@ -174,3 +178,12 @@ CAMOMILLA = {
         }
     },
 }
+
+
+if ENABLE_DEBUG_TOOLBAR:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+    INTERNAL_IPS = [
+        "127.0.0.1",
+        "localhost",
+    ]
--- example/camomilla_example/urls.py
+++ example/camomilla_example/urls.py
@@ -54,3 +54,8 @@ if getattr(settings, "DEBUG", False) or getattr(settings, "DEBUG404", False):
 urlpatterns += i18n_patterns(
     path("", include("camomilla.dynamic_pages_urls")), prefix_default_language=False
 )
+
+if settings.ENABLE_DEBUG_TOOLBAR:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns = debug_toolbar_urls() + urlpatterns
