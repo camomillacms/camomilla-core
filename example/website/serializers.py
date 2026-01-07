0ea3ea408ad82101423b21fde0680f0e420f0b7e
Marco Becattini <20611637+marcobeca@users.noreply.github.com>
2026-01-07 09:39:19 +0100

    test(serializers): Add test standard api serializer (#45)
    * feature: Add test standard api serializer
    
    * feature: Add test standard api serializer - 2

--- example/website/serializers.py
+++ example/website/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from camomilla.serializers.mixins.page import AbstractPageMixin
-from .models import RelatedPageModel, ExposedRelatedPageModel
+from .models import RelatedPageModel, ExposedRelatedPageModel, CustomApiSerializerModel
 
 
 class CustomPageSerializer(AbstractPageMixin):
@@ -22,6 +22,22 @@ class ExposedRelatedPageModelSerializer(AbstractPageMixin):
     class Meta:
         model = ExposedRelatedPageModel
         fields = ["id", "related_pages"]
+
+
+class CustomApiSerializerModelSerializer(AbstractPageMixin):
+    description = serializers.SerializerMethodField()
+    added_field = serializers.SerializerMethodField()
+
+    def get_description(self, obj):
+        return f"{obj.description}-CustomApiSerializer"
+    
+    def get_added_field(self, obj):
+        return "This is an added field from CustomApiSerializerModelSerializer"
+
+    class Meta:
+        model = CustomApiSerializerModel
+        fields = fields = "__all__"
+
 
 
 class InvalidSerializer(serializers.Serializer):
