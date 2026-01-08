f4ddd06f443e6139edf5c98131722145485f0141
bnznamco <gabriele.baldi.01@gmail.com>
2026-01-08 10:46:19 +0100

    fix(views): add permission classes to LanguageViewSet for better access control

--- camomilla/views/languages.py
+++ camomilla/views/languages.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import views
+from rest_framework.permissions import AllowAny
 
 
 class LanguageViewSet(views.APIView):
+    permission_classes = [AllowAny]
+    
     def get(self, request, *args, **kwargs):
         languages = []
         for key, language in settings.LANGUAGES:
